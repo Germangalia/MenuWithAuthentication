f87d993b5045af55a79eb70b22c9f04a252b60cf
Germangalia <germangalia@iesebre.com>
2015-12-14 16:38:35 +0100

    Change namespace with $ php artisan app:name MenuWithAuthentication

---
+++ app/bootstrap/menu.php
@@ -0,0 +1,25 @@
+<?php
+
+//Que tenen els menús?
+
+//Name/tittle
+//on apunta/url
+//te icona?
+//té submenus?
+
+
+//Home
+MenuWithAuthentication::menu
+
+
+
+//Another link
+MenuWithAuthentication::menu
+
+
+
+
+//Multilevel
+MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
